e3434469555cf92d587d212b93543f84a82f2cd1
mikkel-larsen <miklar96@gmail.com>
2021-12-11 12:24:59 +0100

    Included default choice of lambda for diff reg

--- polynomial.py
+++ polynomial.py
@@ -7,14 +7,13 @@ from sklearn.linear_model import LinearRegression
 from simulate_data import BS
 from simulate_data import transform
 
-n = 2000
+n = 200
 strike = 100
 rf_rate = 0.02
 vol = 0.15
 T = 0.3
 
 model = BS(rf_rate, vol, T)
-
 xpoints = transform(np.linspace(20, 180, n), 100)
 sim = model.BS_simulate_endpoint(n, xpoints)
 ypoints = np.maximum(sim - strike, 0)
@@ -38,9 +37,11 @@ def fit_poly(x, y, p, lmbda=0):
     X = design_matrix(x, p)
     X_t = np.transpose(X)
     p = len(X_t)
-    return np.dot(np.linalg.solve(np.matmul(X_t, X) + np.identity(p) * lmbda, X_t), y)
+    return np.dot(np.linalg.solve(np.dot(X_t, X) + np.identity(p) * lmbda, X_t), y)
 
-def fit_poly_diff(x, c, D, p, w):
+def fit_poly_diff(x, c, D, p, lmbda=None):
+    if lmbda == None:
+        lmbda = np.mean(c ** 2) / np.mean(D ** 2)
     X = design_matrix(x, p)
     X_t = np.transpose(X)
     n = len(x)
@@ -49,19 +50,26 @@ def fit_poly_diff(x, c, D, p, w):
     i = [y for y in range(p+1)]
     Y = np.multiply(Y, i)
     Y_t = np.transpose(Y)
-    return np.linalg.solve(w * np.dot(X_t, X) + (1 - w) * np.dot(Y_t, Y), w * np.dot(X_t, c) + (1 - w) * np.dot(Y_t, D))
+    return np.linalg.solve(np.dot(X_t, X) + lmbda * np.dot(Y_t, Y), np.dot(X_t, c) + lmbda * np.dot(Y_t, D))
+    #return np.linalg.solve(w * np.dot(X_t, X) + (1 - w) * np.dot(Y_t, Y), w * np.dot(X_t, c) + (1 - w) * np.dot(Y_t, D))
 
 
 D = [1 if x > strike else 0 for x in sim] * sim / xpoints
 
+print("Y^2 / Z^2: {}".format(np.mean(ypoints ** 2) / np.mean(D ** 2)))
+print("sd(D) / (sd(D) + sd(C): {}".format(np.std(D) / (np.std(D) + np.std(ypoints))))
+
+
+lmbda = np.mean(ypoints ** 2) / np.mean(D ** 2)
+
 plt.subplot(2,1,1)
 plt.scatter(xpoints, ypoints, alpha=0.1)
 plt.plot(xpoints, true_BS(xpoints, rf_rate, vol, T, strike), linestyle='dotted')
-plt.plot(xpoints, poly(xpoints, fit_poly_diff(xpoints, ypoints, D, 4, 0.5)), color='red', linestyle='solid')
+plt.plot(xpoints, poly(xpoints, fit_poly_diff(xpoints, ypoints, D, 7)), color='red', linestyle='solid')
 
 plt.subplot(2,1,2)
 plt.scatter(xpoints, ypoints, alpha=0.1)
 plt.plot(xpoints, true_BS(xpoints, rf_rate, vol, T, strike), linestyle='dotted')
-plt.plot(xpoints, poly(xpoints, fit_poly(xpoints, ypoints, 4)), color='red', linestyle='solid')
+plt.plot(xpoints, poly(xpoints, fit_poly(xpoints, ypoints, 7)), color='red', linestyle='solid')
 
 plt.show()
