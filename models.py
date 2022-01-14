94b812abf595ada50d1f83d3bd7aa72e45fbd851
mikkel-larsen <miklar96@gmail.com>
2022-01-14 17:44:58 +0100

    Implemented AAD for Bachelier and BlackScholes model for pathwise differentials

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 import numpy as np
 from numpy import random
 from scipy.stats import norm
+import tensorflow as tf
 
 class Bachelier:
-    def __init__(self, rf_rate=0, vol=None):
+    def __init__(self, rf_rate=0.0, vol=None):
         self.rf_rate = rf_rate
         self.vol = vol
 
@@ -18,13 +19,40 @@ class Bachelier:
             const = vol * np.sqrt(T)
             return diff * norm.cdf(diff / const) + const * norm.pdf(diff / const)
 
-        Kstar = strike * np.exp(-rf_rate * T)
-        v = vol * np.sqrt((1 - np.exp(-2 * rf_rate * T)) / (2 * rf_rate))
-        diff = (spot - Kstar)
-        return diff * norm.cdf(diff / v) + v * norm.pdf(diff / v)
+        D = (spot * np.exp(rf_rate * T) - strike) / (vol * np.sqrt(T))
+        return np.exp(-rf_rate*T) * vol * np.sqrt(T) * (D * norm.cdf(D) + norm.pdf(D))
 
     @staticmethod
-    def simulate_basket(n, m, min, max, strike, T, w, cov):
+    def simulate_basket(n, m, min, max, strike, T, w, cov, rf_rate=0.0):
+        with tf.GradientTape() as tape:
+            shape = [n, m]
+            # s0 = tf.constant(tf.random.uniform(shape, min, max, dtype=tf.float64))
+            s0 = tf.constant(random.uniform(min, max, shape), dtype=tf.float64)
+            tape.watch(s0)
+            b0 = s0 @ w.reshape(-1, 1)
+            order = tf.reshape(tf.argsort(b0, axis=0), -1)
+            s0 = tf.gather(s0, order)
+            b0 = tf.sort(b0, axis=0)
+
+            sT = s0
+            mean = tf.zeros(m)
+            if rf_rate == 0.0:
+                sT += np.sqrt(T) * random.multivariate_normal(mean, cov, n)
+            else:
+                dt = tf.constant(T / 252, dtype=tf.float64)
+                dt_sq = tf.sqrt(dt)
+                # z = random.multivariate_normal(mean, cov, 252)
+                for i in tf.range(int(252)):
+                    z = random.multivariate_normal(mean, cov, n)
+                    sT += sT * rf_rate * dt + dt_sq * z
+
+            bT = sT @ w.reshape(-1, 1)
+
+            payoff = tf.maximum(bT - strike, 0.0)
+            Z = tape.gradient(payoff, s0)
+            Z = tf.transpose(Z[:, :, np.newaxis], perm=(1, 0, 2))
+        '''
+
         shape = [n, m]  # n simulations of m stocks
         s0 = random.uniform(min, max, shape)  # initialize with uniform dist.
         b0 = np.dot(s0, w)  # initial basket value
@@ -40,14 +68,9 @@ class Bachelier:
 
         Z = np.dot(np.where(bT > strike, 1, 0).reshape(-1, 1), w.reshape(1, -1))  # Pathwise differentials
         Z = np.transpose(Z[:, :, np.newaxis], axes=(1, 0, 2))  # transpose so dimensions fit
+        '''
+        return np.array(s0), np.array(b0), np.array(payoff), np.array(Z)
 
-        return s0, b0, payoff, Z
-
-w = np.array([0.5, 0.5])
-cov = np.array([[200, 0], [0, 200]])
-x, b, y, Z = Bachelier.simulate_basket(5, 2, 0, 10, 5, 1, w, cov)
-print(x)
-print(b)
 
 class BlackScholes:
     def __init__(self, rf_rate, vol):
@@ -58,6 +81,10 @@ class BlackScholes:
         d1 = (np.log(spot / strike) + (self.rf_rate + 0.5 * self.vol ** 2) * T) / (self.vol * np.sqrt(T))
         d2 = d1 - self.vol * np.sqrt(T)
         return spot * norm.cdf(d1) - strike * np.exp(-self.rf_rate * T) * norm.cdf(d2)
+
+    def delta(self, spot, strike, T):
+        d1 = (np.log(spot / strike) + (self.rf_rate + 0.5 * self.vol ** 2) * T) / (self.vol * np.sqrt(T))
+        return norm.cdf(d1)
 
     def simulate_path(self, spot, T):
         dt = T / 252
@@ -79,9 +106,28 @@ class BlackScholes:
         if rf_rate is None:
             rf_rate = self.rf_rate
 
+        s0 = tf.sort(tf.random.uniform([n, 1], min, max, dtype=tf.float64), axis=0)
+        z = tf.random.normal([n, 1], dtype=tf.float64)
+        sT = s0 * tf.exp((rf_rate - vol ** 2 / 2.0) * tf.cast(T, tf.float64) + vol * tf.sqrt(tf.cast(T, tf.float64)) * z)
+
+        payoff = tf.maximum(sT - strike, 0.0)
+        Z = tf.cast(tf.where(sT > strike, 1.0, 0.0), tf.float64) * sT / s0
+        '''
         s0 = np.sort(random.uniform(min, max, n)).reshape(-1, 1)
         z = random.standard_normal(n).reshape(-1, 1)
-        sT = s0 * np.exp((self.rf_rate - self.vol ** 2 / 2) * T + self.vol * np.sqrt(T) * z)
+        sT = s0 * np.exp((rf_rate - vol ** 2 / 2) * T + vol * np.sqrt(T) * z)
+
+        payoff = np.maximum(sT - strike, 0)
+        Z = np.where(sT > strike, 1, 0) * sT / s0
+        '''
+        return np.array(s0), np.array(payoff), np.array(Z)
+
+    @staticmethod
+    def simulate(n, min, max, strike, T, vol, rf_rate):
+
+        s0 = np.sort(random.uniform(min, max, n)).reshape(-1, 1)
+        z = random.standard_normal(n).reshape(-1, 1)
+        sT = s0 * np.exp((rf_rate - vol ** 2 / 2) * T + vol * np.sqrt(T) * z)
 
         payoff = np.maximum(sT - strike, 0)
         Z = np.where(sT > strike, 1, 0) * sT / s0
