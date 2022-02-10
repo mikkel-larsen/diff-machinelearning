bed80169b01af053ba24c8c67052e966a4116030
mikkel-larsen <miklar96@gmail.com>
2022-02-10 14:44:34 +0100

    Applied make-up

--- polynomial.py
+++ polynomial.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 
-from models import Bachelier, BlackScholes, Bachelier_eulerScheme, BlackScholes_eulerScheme, simulate_data
-from options import Call, Call_basket
+from models import Bachelier_helper, BlackScholes_helper, Bachelier_eulerScheme, BlackScholes_eulerScheme, simulate_data
+from options import Call, Call_basket, Call_geometric
 
 class PolyReg:
     def __init__(self, x, y, p):
@@ -20,9 +20,19 @@ class PolyReg:
 
     def differential(self, x):
         X = self.poly_features.fit_transform(x)
-        powers = self.poly_features.powers_
+        powers = self.poly_features.powers_#.reshape(-1, 1)
         D = np.transpose(np.transpose(powers)[:, :, np.newaxis] * np.transpose(X), axes=(0, 2, 1)) / \
             np.transpose(x[:, :, np.newaxis], axes=(1, 0, 2))
+
+        return np.sum(np.dot(D, self.params), axis=0).reshape(-1, 1)
+        #return np.dot(D, self.params).reshape(-1, 1)
+
+    def integrated(self, x):
+        X = self.poly_features.fit_transform(x)
+        powers = self.poly_features.powers_ + 1
+        D = np.transpose(np.transpose(X) / np.transpose(powers)[:, :, np.newaxis], axes=(0, 2, 1)) * \
+            np.transpose(x[:, :, np.newaxis], axes=(1, 0, 2))
+
         return np.dot(D, self.params).reshape(-1, 1)
 
     def fit(self, lmbda=0):
@@ -33,7 +43,7 @@ class PolyReg:
 
     def fit_differential(self, Z, lmbda=None):
         if lmbda is None:
-            lmbda = np.dot(np.transpose(ypoints), ypoints) / np.dot(np.transpose(Z[0, :, :]), Z[0, :, :])
+            lmbda = np.dot(np.transpose(y), y) / np.dot(np.transpose(Z[0, :, :]), Z[0, :, :])
 
         X_t = np.transpose(self.X)
         powers = self.poly_features.powers_
@@ -45,136 +55,4 @@ class PolyReg:
         p2 = np.dot(X_t, self.y) + lmbda * np.sum(np.matmul(D_t, Z), axis=0)
         self.params = np.linalg.solve(p1, p2)
         return self.params
-
-
-# Set parameters
-n = 1000  # Number of samples
-n_test = 1000  # Number of samples for testing fit
-m = 5  # Number of stocks in the basket
-w = np.array([1/m for _ in range(m)])  # Weight of individual stock in basket
-strike = 100  # Strike of basket
-rf_rate = 0.0  # Risk-free rate (0 to easily simulate in the Bachelier model)
-vol = 50  # Volatility in the model
-cov = np.identity(m) * (vol ** 2)  # Covariance matrix governing stocks in basket
-basket_vol = np.sqrt(np.dot(np.transpose(w), np.dot(cov, w)))
-T = 0.3  # Time-to-maturity of basket option
-spot_rng = [50, 150]
-
-call = Call(strike, T)
-basket = Call_basket(strike, T, w)
-
-Bach = Bachelier(rf_rate, vol)  # Helper for analytic price etc.
-Bach_model = Bachelier_eulerScheme(rf_rate, cov)
-
-xpoints, ypoints, Z = simulate_data([n, m], spot_rng, basket, Bach_model)
-xpoints_basket = np.reshape(xpoints @ w.T, (-1, 1))
-
-xpoints_test, ypoints_test, Z_test = simulate_data([n_test, m], spot_rng, basket, Bach_model)
-xpoints_basket_test = np.reshape(xpoints_test @ w.T, (-1, 1))
-
-Z_perm = np.transpose(Z[:, :, np.newaxis], axes=(1, 0, 2))
-lmbda = np.dot(np.transpose(ypoints), ypoints) / np.dot(np.transpose(Z_perm[0, :, :]), Z_perm[0, :, :])  # Lambda for regularization, weighted by variation of term
-print("lambda: {}".format(lmbda))
-
-
-# Black-Scholes simulated data for regression of options
-vol = 0.20  # Volatility in the model (way lower in the Black-Scholes model)
-rf_rate = 0.02  # Risk-free rate (0 to easily simulate in the Bachelier model)
-
-BS = BlackScholes(rf_rate, vol)  # Helper for analytic price etc.
-BS_model = BlackScholes_eulerScheme(rf_rate, vol)
-xpoints_bs, ypoints_bs, D = simulate_data(n, spot_rng, call, BS_model)
-
-order = np.argsort(xpoints_bs, axis=0).flatten()
-xpoints_bs = xpoints_bs[order]
-ypoints_bs = ypoints_bs[order]
-D = D[order]
-
-lmbda_BS = np.mean(ypoints_bs ** 2) / np.mean(D ** 2)
-
-p = 6  # Highest number of polynomial for polynomial regression
-
-# Polynomial regression objects for estimating Black-Scholes price in 1D
-poly_BS = PolyReg(xpoints_bs, ypoints_bs, p)
-poly_BS.fit()
-
-poly_BS_l2reg = PolyReg(xpoints_bs, ypoints_bs, p)
-poly_BS_l2reg.fit(lmbda_BS)
-
-poly_BS_dreg = PolyReg(xpoints_bs, ypoints_bs, p)
-poly_BS_dreg.fit_differential(D, lmbda_BS)
-
-# Polynomial regression objects for estimating Bachelier price in multi-dim
-poly = PolyReg(xpoints, ypoints, p)
-poly.fit()
-
-poly_l2reg = PolyReg(xpoints, ypoints, p)
-poly_l2reg.fit(lmbda)
-
-poly_dreg = PolyReg(xpoints, ypoints, p)
-poly_dreg.fit_differential(Z, lmbda)
-
-
-#########--------.....Plotting.....--------#########
-'''
-# BLACK-SCHOLES PLOT
-# Classic polynomial regression
-plt.subplot(3, 2, 1)
-plt.scatter(xpoints_bs, ypoints_bs, alpha=0.1, s=20)
-plt.plot(xpoints_bs, poly_BS.predict(xpoints_bs), color='red')
-plt.plot(xpoints_bs, BS.call_price(xpoints_bs, strike, T), color='green', linestyle='dotted')
-
-plt.subplot(3, 2, 2)
-plt.plot(xpoints_bs, poly_BS.differential(xpoints_bs), color='red')
-plt.plot(xpoints_bs, BS.call_delta(xpoints_bs, strike, T), color='green', linestyle='dotted')
-
-# Polynomial regression with L2-regularization
-plt.subplot(3, 2, 3)
-plt.scatter(xpoints_bs, ypoints_bs, alpha=0.1, s=20)
-plt.plot(xpoints_bs, poly_BS_l2reg.predict(xpoints_bs), color='red')
-plt.plot(xpoints_bs, BS.call_price(xpoints_bs, strike, T), color='green', linestyle='dotted')
-
-plt.subplot(3, 2, 4)
-plt.plot(xpoints_bs, poly_BS_l2reg.differential(xpoints_bs), color='red')
-plt.plot(xpoints_bs, BS.call_delta(xpoints_bs, strike, T), color='green', linestyle='dotted')
-
-# Polynomial regression with differential regularization
-plt.subplot(3, 2, 5)
-plt.scatter(xpoints_bs, ypoints_bs, alpha=0.1, s=20)
-plt.plot(xpoints_bs, poly_BS_dreg.predict(xpoints_bs), color='red')
-plt.plot(xpoints_bs, BS.call_price(xpoints_bs, strike, T), color='green', linestyle='dotted')
-
-plt.subplot(3, 2, 6)
-plt.plot(xpoints_bs, poly_BS_dreg.differential(xpoints_bs), color='red')
-plt.plot(xpoints_bs, BS.call_delta(xpoints_bs, strike, T), color='green', linestyle='dotted')
-
-plt.show()
-
-'''
-
-# PLOT FOR BASKET
-# Classic polynomial regression
-plt.subplot(3, 1, 1)
-plt.scatter(xpoints_basket, ypoints, alpha=0.1, s=20)
-plt.plot(xpoints_basket_test, Bach.call_price(xpoints_basket_test, strike, T, basket_vol), color='green', linestyle='none', marker='x', ms=3, mew=0.5, alpha=0.4)
-plt.plot(xpoints_basket_test, poly.predict(xpoints_test), color='red', alpha=0.4, marker="x", ls='none', ms=3, mew=0.5)
-plt.ylim(-5, 50)
-
-# Polynomial regression with L2-regularization
-plt.subplot(3, 1, 2)
-plt.scatter(xpoints_basket, ypoints, alpha=0.1, s=20)
-plt.plot(xpoints_basket_test, Bach.call_price(xpoints_basket_test, strike, T, basket_vol), color='green', linestyle='none', marker='x', ms=3, mew=0.5, alpha=0.4)
-plt.plot(xpoints_basket_test, poly_l2reg.predict(xpoints_test), color='red', alpha=0.4, marker="x", ls='none', ms=3, mew=0.5)
-plt.ylim(-5, 50)
-
-# Polynomial regression with differential regularization
-plt.subplot(3, 1, 3)
-plt.scatter(xpoints_basket, ypoints, alpha=0.1, s=20)
-plt.plot(xpoints_basket_test, Bach.call_price(xpoints_basket_test, strike, T, basket_vol), color='green', linestyle='none', marker='x', ms=3, mew=0.5, alpha=0.4)
-plt.plot(xpoints_basket_test, poly_dreg.predict(xpoints_test), color='red', alpha=0.4, marker="x", ls='none', ms=3, mew=0.5)
-plt.ylim(-5, 50)
-
-# plt.savefig('foo.png', dpi=1000, bbox_inches='tight')
-
-plt.show()
 
