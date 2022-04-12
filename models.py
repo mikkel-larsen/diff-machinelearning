fd19fa98dc7fc2785229fc9b49fd95e7a2905c32
mikkel-larsen <miklar96@gmail.com>
2022-04-12 10:38:42 +0200

    Supports Quasi-MC

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ from scipy.stats.qmc import MultivariateNormalQMC
 import tensorflow as tf
 from scipy.optimize import root_scalar
 from scipy.integrate import quad
+import warnings
 
 class Multivariate_0mean_Normal:
     def __init__(self, dim=1, var=None, antithetic=False, quasi_mc=False):
@@ -27,8 +28,16 @@ class Multivariate_0mean_Normal:
             self.gen = multivariate_normal(np.zeros(self.dim), self.var)
 
     def generate_random(self, n=1):
+        if n < 1:
+            raise ValueError(f'n must be a positive integer, got {n}')
         if self.quasi_mc:
-            z = self.gen.random(n).reshape((n, self.dim))
+            if ((n - 1) & n) != 0:
+                n_to_nearest_power_of_2 = 1 << n.bit_length()
+                z = self.gen.random(n_to_nearest_power_of_2)[:n].reshape(n, self.dim)
+                warnings.warn(f'Sobol algorithm relies on input being a power of 2, got {n}. Use {n_to_nearest_power_of_2} for proper results.')
+            else:
+                z = self.gen.random(n).reshape(n, self.dim)
+
             perm = np.arange(n)
             self.shuffler.shuffle(perm)
             return z[perm]
@@ -416,73 +425,6 @@ class Bachelier_helper:
         d = (spot - strike) / vol / np.sqrt(T)
         return norm.cdf(d)
 
-    def simulate_price_path(self, n, spot, T, n_yearly_updates=252):
-        if np.size(spot) == 2:
-            spot_min, spot_max = spot
-        else:
-            spot_min = spot
-            spot_max = spot
-
-        if T < 1:
-            end = n_yearly_updates  # Update 252 times (default)
-            dt = T / n_yearly_updates
-        else:
-            end = int(T * n_yearly_updates)  # Update once every day (default)
-            dt = 1 / n_yearly_updates
-
-        spots = np.zeros((n, end))
-        spots[:, 0] = random.uniform(spot_min, spot_max, n)
-
-        z = random.standard_normal((n, end))
-        dt_sqrt = np.sqrt(dt)
-        for i in range(1, end):
-            spots[:, i] = spots[:, i - 1] * (1 + self.rf_rate * dt) + self.vol * dt_sqrt * z[:, i]
-
-        return spots
-
-    def simulate_basket(self, n, m, rng, option, w, cov, seed=None):
-        if seed is not None:
-            tf.random.set_seed(seed)
-        if np.size(rng) == 2:
-            spot_min, spot_max = rng
-        else:
-            spot_min = rng
-            spot_max = rng
-
-        with tf.GradientTape() as tape:
-            T = option.T
-            shape = [n, m]
-            s0 = tf.random.uniform(shape, spot_min, spot_max, dtype=tf.float64)
-            tape.watch(s0)
-            b0 = s0 @ w.reshape(-1, 1)
-            order = tf.reshape(tf.argsort(b0, axis=0), -1)
-            s0 = tf.gather(s0, order)
-            b0 = tf.sort(b0, axis=0)
-
-            sT = s0
-            mean = tf.zeros(m)
-            if self.rf_rate == 0.0:
-                sT += np.sqrt(T) * random.multivariate_normal(mean, cov, n)
-            else:
-                if T < 1:
-                    end = tf.constant(252, dtype=tf.int64)  # Update 252 times
-                    dt = tf.constant(T / 252, dtype=tf.float64)
-                else:
-                    end = tf.constant(T * 252, dtype=tf.int64)  # Update once every day times
-                    dt = tf.constant(1 / 252, dtype=tf.float64)
-
-                dt_sq = tf.sqrt(dt)
-                for _ in tf.range(end):
-                    z = random.multivariate_normal(mean, cov, n)
-                    sT += sT * self.rf_rate * dt + dt_sq * z
-
-            bT = sT @ w.reshape(-1, 1)
-
-            payoff = option.payoff(bT)
-            Z = tape.gradient(payoff, s0)
-
-        return np.array(s0), np.array(b0), np.array(payoff), np.array(Z)
-
 
 class BlackScholes_helper:
     def __init__(self, rf_rate, vol):
@@ -527,103 +469,6 @@ class BlackScholes_helper:
         return np.exp(-self.rf_rate * T) * (norm.pdf((np.log(spot / strike) +
                (self.rf_rate + 0.5 * self.vol**2) * T) / (self.vol * np.sqrt(T)) -
                self.vol * np.sqrt(T)) * (1 / strike / (spot / strike) / (self.vol * np.sqrt(T))))
-
-
-    def simulate_price_path(self, n, spot, T, n_yearly_updates=252, seed=None):
-        random.seed(seed)
-        if np.size(spot) == 2:
-            spot_min, spot_max = spot
-        else:
-            spot_min = spot
-            spot_max = spot
-
-        if T < 1:
-            end = n_yearly_updates  # Update 252 times (default)
-            dt = T / n_yearly_updates
-        else:
-            end = int(T * n_yearly_updates)  # Update once every day (default)
-            dt = 1 / n_yearly_updates
-
-        spots = np.zeros((n, end))
-        spots[:, 0] = random.uniform(spot_min, spot_max, n)
-        z = random.standard_normal((n, end))
-        dt_sqrt = np.sqrt(dt)
-        for i in range(1, end):
-            spots[:, i] = spots[:, i - 1] * (1 + self.rf_rate * dt + self.vol * dt_sqrt * z[:, i])
-
-        return spots
-
-    def simulate_data(self, n, rng, option, seed=None):
-        if seed is not None:
-            tf.random.set_seed(seed)
-
-        T = option.T
-
-        if np.size(rng) == 2:
-            spot_min, spot_max = rng
-        else:
-            spot_min = rng
-            spot_max = rng
-
-        simple = False
-        if hasattr(option, 'is_simple'):
-            if option.is_simple:
-                simple = True
-
-        if simple:
-            with tf.GradientTape() as tape:
-                s0 = tf.sort(tf.random.uniform([n, 1], spot_min, spot_max, dtype=tf.float64), axis=0)
-                tape.watch(s0)
-                z = tf.random.normal([n, 1], dtype=tf.float64)
-                sT = s0 * tf.exp((self.rf_rate - self.vol ** 2 / 2.0) * tf.cast(T, tf.float64) +
-                                 self.vol * tf.sqrt(tf.cast(T, tf.float64)) * z)
-                payoff = option.payoff(sT)
-            Z = tape.gradient(payoff, s0)
-        else:
-            if T < 1:
-                end = 252  # Update 252 times (default)
-                dt = T / 252
-            else:
-                end = int(T * 252)  # Update once every day (default)
-                dt = 1 / 252
-
-            with tf.GradientTape() as tape:
-                s0 = tf.sort(tf.random.uniform([1, n], spot_min, spot_max, dtype=tf.float64), axis=0)
-                tape.watch(s0)
-                z = random.standard_normal((n, end))
-                dt_sqrt = np.sqrt(dt)
-                sT = s0
-                price_path = [sT]
-                for i in range(end):
-                    sT += sT * (self.rf_rate * dt + self.vol * dt_sqrt * z[:, i])
-                    price_path.append(sT)
-
-                payoff = option.payoff(price_path)
-            Z = tape.gradient(payoff, s0)
-            s0 = tf.reshape(s0, (-1, 1))
-            payoff = tf.reshape(payoff, (-1, 1))
-            Z = tf.reshape(Z, (-1, 1))
-
-        return np.array(s0), np.array(payoff), np.array(Z)
-
-    @staticmethod
-    def simulate(n, min, max, option, vol, rf_rate, seed=None):
-        if seed is not None:
-            tf.random.set_seed(seed)
-
-        T = option.T
-
-        with tf.GradientTape() as tape:
-            s0 = tf.sort(tf.random.uniform([n, 1], min, max, dtype=tf.float64), axis=0)
-            tape.watch(s0)
-            z = tf.random.normal([n, 1], dtype=tf.float64)
-            sT = s0 * tf.exp(
-                (rf_rate - vol ** 2 / 2.0) * tf.cast(T, tf.float64) + vol * tf.sqrt(tf.cast(T, tf.float64)) * z)
-            payoff = option.payoff(sT)
-
-        Z = tape.gradient(payoff, s0)
-
-        return s0, payoff, Z
 
 
 def transform(x, K, b=None):
